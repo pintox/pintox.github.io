0439a9770a1d86e82a0ee6ac13f36ef9ee8dad64
Pinto Xavier <33973899+pinto-xavier-dev@users.noreply.github.com>
2023-09-09 21:30:50 +0530

    testing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
